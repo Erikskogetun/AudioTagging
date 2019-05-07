0aa6864810b60491cfec64bd6d57af344928d913
Ryan Davis <rsd9@uw.edu>
2019-05-07 14:39:44 +0200

    changed sox path to match vm installation path

--- ryan/data_synthesis.py
+++ ryan/data_synthesis.py
@@ -42,7 +42,7 @@ def generate_data(raw_data_path, output_path, chunk_size=128, test_frac=0.2, rem
             if remove_silence:
                 # file = file_path
                 aug_cmd = "norm -0.1 silence 1 0.025 0.15% norm -0.1 reverse silence 1 0.025 0.15% reverse"
-                os.system("sox %s %s %s" % (file_path, aug_audio_file, aug_cmd))
+                os.system("../../sox-14.4.2/src/sox %s %s %s" % (file_path, aug_audio_file, aug_cmd))
                 # os.system('../../sox-14.4.2/sox ' + file + ' ' + aug_audio_file + ' ' + aug_cmd)
 
                 assert os.path.exists(aug_audio_file), "SOX Problem ... clipped wav does not exist!"
