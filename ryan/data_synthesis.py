e95ee807209c396cf1de90f485b1ceed242ae0ab
JimSmit <38352013+JimSmit@users.noreply.github.com>
2019-05-21 22:04:18 +0200

    Update data_synthesis.py

--- ryan/data_synthesis.py
+++ ryan/data_synthesis.py
@@ -211,11 +211,11 @@ def _remove_silence(file_path, aug_audio_file):
     aug_cmd = "norm -0.1 silence 1 0.025 0.15% norm -0.1 reverse silence 1 0.025 0.15% reverse"
     os.system("../../sox-14.4.2/src/sox %s %s %s" % (file_path, aug_audio_file, aug_cmd))
 
-    if os.path.exists(aug_audio_file):
-      print(good)
-    else:
-      continue
-
+    #if os.path.exists(aug_audio_file):
+     # print(good)
+    #else:
+     # continue
+    assert os.path.exists(aug_audio_file), "SOX Problem ... clipped wav does not exist! (while removing silence)"
 
 # TODO: this may cause clipping as files were normalized to -0.1 in silence removal stage.
 # TODO: Jim or someone: listen to some of the generated mixes and make sure there isn't clipping present when two or
@@ -227,12 +227,12 @@ def _sum_audio(audio_files, aug_audio_file):
     cmd = cmd + aug_audio_file
     os.system(cmd)
     
-    if os.path.exists(aug_audio_file):
-      print(good)
-    else:
-      continue
+    #if os.path.exists(aug_audio_file):
+     # print(good)
+    #else:
+      #continue
 
-   #assert os.path.exists(aug_audio_file), "SOX Problem ... clipped wav does not exist! (while removing silence)"
+    assert os.path.exists(aug_audio_file), "SOX Problem ... clipped wav does not exist! (while removing silence)"
 
 
 def _spectrogram_to_chunks(spectrogram, chunk_size):
